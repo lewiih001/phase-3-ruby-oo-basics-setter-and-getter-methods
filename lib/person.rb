278758eda6a4c449d18a98b1a1864a9b6a56c460
lewiih001 <lewismunyambu@gmail.com>
2022-10-23 08:09:28 +0300

    hacked the lab

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,22 @@
 class Person
 
-end
+    # setter method
+    def name=(name)
+      @name = name
+    end
+    
+    def job
+        @job
+    end
+  def job=(job)
+    @job=job
+  end
+    # getter method
+    def name
+      @name
+    end
+  
+  end
+
+#   lewis = "singer"
+#   lewis.job = "singer"
